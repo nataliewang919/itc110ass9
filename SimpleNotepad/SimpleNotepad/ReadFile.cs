a2128944f2b0433c8086b811682f185a47e67e59
nataliewang919 <hwang031@seattlecentral.edu>
2016-11-29 12:05:46 -0800

    Add files via upload

---
+++ SimpleNotepad/SimpleNotepad/ReadFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SimpleNotepad
+{
+    public class ReadFile
+    {
+        private StreamReader reader;
+        private string filepath = null;
+
+        public ReadFile(string path)
+        {
+            filepath = path;
+        }
+        public string GetFile()
+        {
+            string line = null;
+            try
+            {
+                reader = new StreamReader(filepath);
+                line = reader.ReadToEnd();
+            }
+            catch(FileNotFoundException fnfex)
+            {
+                throw fnfex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return line;
+        }
+    }
+}
---
+++ SimpleNotepad/SimpleNotepad/WriteFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SimpleNotepad
+{
+    class WriteFile
+    {
+        private StreamWriter writer;
+        string filepath = null;
+
+        public WriteFile(string path)
+        {
+            try
+            {
+                filepath = path;
+                writer = new StreamWriter(path, true);
+            }
+          
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void AddText(string line)
+        {
+            writer.Write(line);
+        }
+
+        public void CloseFile()
+        {
+            writer.Close();
+        }
+    }
+}
